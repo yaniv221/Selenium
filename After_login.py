209e44ba21382cdd5207da0ce7afd865093f64ad
yaniv221 <44192476+yaniv221@users.noreply.github.com>
2018-11-15 17:40:26 +0200

    Add files via upload
    new version

--- After_login.py
+++ After_login.py
@@ -1,17 +1,23 @@
 
+import ChromeWebDriver
 
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
-driver = webdriver.Chrome("C:/python/chromedriver.exe")
 
 
-def _community():ww
+def _community():
+    driver = webdriver.Chrome("C:/Users/yanivova/PycharmProjects/Web_Automation/chromedriver.exe")
 
 
-driver.get('https://us.toluna.com/#/')
 
-element = driver.find_element_by_xpath(xpath='//*[@id="jsddm"]/li[1]/a/span[2]')
-element.click()
+    driver.get('https://us.toluna.com/Recent')
 
-element = driver.find_element_by_xpath(xpath='//*[@id="jsddm"]/li[1]/div/a[2]')
-element.click()
+    element = driver.find_element_by_xpath(xpath='//*[@id="jsddm"]/li[1]/a/span[2]')
+    element.click()
+
+    driver.implicitly_wait(30)
+    element = driver.find_element_by_xpath(xpath='//*[@id="jsddm"]/li[1]/div/a[2]')
+    element.click()
+if __name__== "__main__":
+    _community()
+
--- ChromeWebDriver.py
+++ ChromeWebDriver.py
@@ -1,31 +1,42 @@
 import time
+import After_login
 from selenium import webdriver
+"""""
 from After_login import  _community
 from selenium.webdriver.common.keys import Keys
-driver = webdriver.Chrome("C:/python/chromedriver.exe")
-driver.get("http://www.google.com/")
+"""""
 
-#open tab
 
-# You can use (Keys.CONTROL + 't') on other OSs
 
-# Load a page
-driver.get('https://us.toluna.com/#/')
-# Make the tests..
-driver.implicitly_wait(30)
-element = driver.find_element_by_xpath(xpath='//*[@id="root"]/nav/div/div[2]/span/a/span[2]')
-element.click()
 
-inputElement = driver.find_element_by_id(id_='Username')
-inputElement.send_keys('yan257')
 
-inputElement = driver.find_element_by_id(id_='Password')
-inputElement.send_keys('adinoa02')
 
-element = driver.find_element_by_xpath(xpath='//html/body/div[6]/div[2]/div/div/div[2]/div/button')
-element.click()
 
-driver.implicitly_wait(99)
+
+def login():
+    # Load a page
+    driver = webdriver.Chrome("C:/Users/yanivova/PycharmProjects/Web_Automation/chromedriver.exe")
+    driver.get("http://www.google.com/")
+
+    # open tab
+    driver.get('https://us.toluna.com/#/')
+    # Make the tests..
+    driver.implicitly_wait(30)
+    element = driver.find_element_by_xpath(xpath='//*[@id="root"]/nav/div/div[2]/span/a/span[2]')
+    element.click()
+
+    inputElement = driver.find_element_by_id(id_='Username')
+    inputElement.send_keys('yan257')
+
+    inputElement = driver.find_element_by_id(id_='Password')
+    inputElement.send_keys('adinoa02')
+
+    element = driver.find_element_by_xpath(xpath='//html/body/div[6]/div[2]/div/div/div[2]/div/button')
+    element.click()
+
+    driver.implicitly_wait(99)
+
+
 
 
 
